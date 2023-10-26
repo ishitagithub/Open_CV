d0b57c055077ab4b4919b374a4c52654c6bc147b
Ishita Agarwal <90623408+ishitagithub@users.noreply.github.com>
2023-10-27 00:00:25 +0530

    Update crypto.py

--- crypto.py
+++ crypto.py
@@ -15,11 +15,10 @@ initial_balance = st.sidebar.number_input("Initial Balance (USDT)", min_value=1,
 trade = st.sidebar.text_input("Trading Pair (e.g., BTC/USDT)", value="BTC")
 short_window = st.sidebar.number_input("Short Window (days)", min_value=1, value=50)
 long_window = st.sidebar.number_input("Long Window (days)", min_value=1, value=200)
-with top_bar:
-    st.write(f"Initial Balance: {initial_balance} USDT")
-    st.write(f"Trading Pair: {trade}")
-    st.write(f"Short Window: {short_window} days")
-    st.write(f"Long Window: {long_window} days")
+st.write(f"Initial Balance: {initial_balance} USDT")
+st.write(f"Trading Pair: {trade}")
+st.write(f"Short Window: {short_window} days")
+st.write(f"Long Window: {long_window} days")
 st.write("SMA50: 27692.7166, SMA200: 28160.9183")
 
 st.write("Neither buy nor sell")
