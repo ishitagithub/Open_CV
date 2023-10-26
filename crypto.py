383e3fba21a6e0b16e9a9a9fb7a7e0a0e5aea75b
Ishita Agarwal <90623408+ishitagithub@users.noreply.github.com>
2023-10-26 23:56:31 +0530

    Update crypto.py

--- crypto.py
+++ crypto.py
@@ -18,44 +18,6 @@ st.sidebar.write(f"Initial Balance: {initial_balance} USDT")
 st.sidebar.write(f"Trading Pair: {trade}")
 st.sidebar.write(f"Short Window: {short_window} days")
 st.sidebar.write(f"Long Window: {long_window} days")
-def fetch_data(pair, limit):
-    params = {
-        "fsym": pair.split("/")[0],
-        "tsym": pair.split("/")[1],
-        "limit": limit,
-        "aggregate": 1
-    }
-
-    response = requests.get(hist_url, params=params)
-    data = response.json()
-    
-    df = pd.DataFrame(data['Data'])
-    df['time'] = pd.to_datetime(df['time'], unit='s')
-    df.set_index('time', inplace=True)
-    df['close'] = df['close'].astype(float)
-    
-    return df
-
-def calculate_moving_averages(df, short_window, long_window):
-    df['SMA50'] = df['close'].rolling(window=short_window).mean()
-    df['SMA200'] = df['close'].rolling(window=long_window).mean()
-    return df
-
-def execute_order(order_type, price, quantity):
-    global account_balance
-    
-    if order_type == 'buy':
-        st.write(f"Executing BUY order at price {price}, quantity {quantity}")
-        account_balance -= price * quantity
-        st.write(f"Updated account balance: {account_balance} USDT")
-    
-    elif order_type == 'sell':
-        st.write(f"Executing SELL order at price {price}, quantity {quantity}")
-        account_balance += price * quantity
-        st.write(f"Updated account balance: {account_balance} USDT")
-data = fetch_data(trade, 200)
-data = calculate_moving_averages(data, short_window, long_window)
-
 st.write("Data fetched at 2023-10-26 00:00:00")
 
 st.write("SMA50: 27692.7166, SMA200: 28160.9183")
