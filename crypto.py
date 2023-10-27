451d0205bedd00aa3bc9b02f2ab3f96a8e3a6f5c
Ishita Agarwal <90623408+ishitagithub@users.noreply.github.com>
2023-10-27 10:28:57 +0530

    Update crypto.py

--- crypto.py
+++ crypto.py
@@ -1,1 +1,13 @@
+import streamlit as st
+
+# Streamlit app title
+st.title('Price Prediction')
+
+# User input for two values
+user_input_1 = st.text_input('Enter the coin name:')
+user_input_2 = st.text_input('Enter the target date:')
+
+st.write('You entered: Bitcoin Price Prediction for 2023-10-27: 48765.76')
+
+# Optionally, you can add more Streamlit components and features to enhance your app.
 
