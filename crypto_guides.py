8255d15876676fac006dbee3f806f5dd46149de0
Ishita Agarwal <90623408+ishitagithub@users.noreply.github.com>
2023-11-01 22:12:33 +0530

    Update crypto_guides.py

--- crypto_guides.py
+++ crypto_guides.py
@@ -1,14 +1,1 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-import pandas as pd
-
-# Streamlit UI
-st.title('Stock Price Prediction')
-
-# Date input
-selected_date = st.date_input('Select a date', pd.to_datetime('2023-01-01'))
-
-st.write("You should sell your stocks.")
-st.markdown('Ishita Agarwal')
 
